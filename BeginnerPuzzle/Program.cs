0c945a4320cffa85725cb90db631a06e2393c736
csharppuzzles <162582538+csharppuzzles@users.noreply.github.com>
2024-06-10 10:25:25 +0800

    Update Program.cs

--- BeginnerPuzzle/Program.cs
+++ BeginnerPuzzle/Program.cs
@@ -110,14 +110,14 @@ static void PuzzleC()
 
 // Run the puzzles
 
-    ExampleA();
-    //PuzzleA();
+    //ExampleA();
+    PuzzleA();
 
-    ExampleB();
-    //PuzzleB();
+    //ExampleB();
+    PuzzleB();
 
-    ExampleC();
-    //PuzzleC();
+    //ExampleC();
+    PuzzleC();
 
 
 
