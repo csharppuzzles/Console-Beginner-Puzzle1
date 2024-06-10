11a7f17acf359b0539f53a7299cc5652a19a816b
scottishsmile <20988234-scottishsmile@users.noreply.gitlab.com>
2024-06-10 09:59:12 +0800

    Initial Commit

---
+++ BeginnerPuzzle/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+static void ExampleA()
+{
+    Console.WriteLine("\nExample A");
+    Console.WriteLine("~~~~~~~~~");
+
+    Console.WriteLine("\nWould you rather be a Parking Enforcement Officer or a Clown?: ");
+    string? choice = Console.ReadLine();
+
+    // String concatenation
+    // Add the variable "choice" onto the end of the string
+    Console.WriteLine("You chose: " + choice);
+    Console.WriteLine("It was a trick question, they are BOTH clowns");
+}
+
+
+// Puzzle A
+// Write a program that asks for your favourite color.
+// It should output "Your favourite color is " and then their fav color
+// Use String Interpolation to print the answer, not string concatenation as shown above.
+// There are two types: $" blah blah blah {var}" and string.Format()
+
+/*
+static void PuzzleA()
+{
+    Console.WriteLine("\nPuzzle A");
+    Console.WriteLine("~~~~~~~~~");
+
+}
+*/
+
+
+static void ExampleB()
+{
+    Console.WriteLine("\nExample B");
+    Console.WriteLine("~~~~~~~~~");
+
+    string message = "I wish I had chainsaws for hands, or at least nuclear arms.";
+    Console.WriteLine(message);
+    Console.WriteLine("Converted to all lowercase: " + message.ToLower());
+}
+
+
+// Puzzle B
+// Write a program that displays the message
+// "I was too sick to do my homework."
+// Then display this same message in all capitals (uppercase).
+// Use string interpolation again, not concatenation as shown in Example B.
+
+/*
+static void PuzzleB()
+{
+    Console.WriteLine("\nPuzzle B");
+    Console.WriteLine("~~~~~~~~~");
+
+}
+*/
+
+
+static void ExampleC()
+{
+    Console.WriteLine("\nExample C");
+    Console.WriteLine("~~~~~~~~~");
+
+    Console.WriteLine("What's your favourite food?: ");
+    string? favFood = Console.ReadLine();
+
+    Console.WriteLine("What's your favourite drink?: ");
+    string? favDrink = Console.ReadLine();
+
+    Console.WriteLine($"If I was to take you on a date, we would have {favFood} and {favDrink}.");
+    Console.WriteLine($"But you are far too ugly for me to ask you on a date.");
+
+}
+
+
+// Puzzle C
+// Ask the user to input a folder name
+// Then ask the user to input a file name
+// Display the file path "C:\{folderName}\{fileName}.txt" using the user's inputs
+// You will need to learn how to escape the backslash characters or use verbatim strings.
+/*
+static void PuzzleC()
+{
+    Console.WriteLine("\nPuzzle C");
+    Console.WriteLine("~~~~~~~~~");
+
+}
+*/
+
+
+
+// Run the puzzles
+
+    ExampleA();
+    //PuzzleA();
+
+    ExampleB();
+    //PuzzleB();
+
+    ExampleC();
+    //PuzzleC();
+
+
+
+Console.WriteLine("\n Press enter to exit the program...");
+Console.ReadLine();                                         // Keeps the console app window open until you hit enter
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Console-Beginner-Puzzle1
-Writeline and Readline
+# Csharp Console  Beginners Puzzle 1
+### Lets do some console app puzzles. Most beginners start off writing console apps, so it is a good way to make sure we cover the basics.
+
+### View the YouTube Video: 
+
+
+## Puzzles
+In Program.cs, I have commented out some starting code for these puzzles. <br />
+Write your code in these sections and uncomment them. <br />
+I have provided some examples of similar functions to what each puzzle is asking for. Learn to read the code and modify it. <br />
+
+At the very bottom, I have listed the functions. Comment and uncomment them, to run them one at a time.
+
+```
+// Run the puzzles
+
+    ExampleA();
+    //PuzzleA();
+
+    ExampleB();
+    //PuzzleB();
+
+    ExampleC();
+    //PuzzleC();
+```
+
+### A -  Your favourite color
+Write a program that asks for your favourite color. <br />
+It should output "Your favourite color is " and then their fav color <br />
+Use String Interpolation to print the answer, not string concatenation as shown above. <br />
+There are two types: $" blah blah blah {var}" and string.Format() <br />
+
+
+### B - I was too sick to do my homework
+Write a program that displays the message "I was too sick to do my homework." <br />
+Then display this same message in all capitals (uppercase). <br />
+Use string interpolation again, not concatenation as shown in Example B. <br />
+
+
+### C – Folder Names
+Ask the user to input a folder name <br />
+Then ask the user to input a file name <br />
+Display the file path "C:\{folderName}\{fileName}.txt" using the user's inputs <br />
+You will need to learn how to escape the backslash characters or use verbatim strings. <br />
+
+
+# Good Luck!
